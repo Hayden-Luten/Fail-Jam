5cd414a0f5a8f03b688ce0888b42087c14f080e8
Hayden-Luten <144058205+Hayden-Luten@users.noreply.github.com>
2023-11-23 19:30:05 -0800

    update
    added physics for platforms
    tweaked gravity and jump power
    added respawn after falling
    resized camera

--- player/charMovement.gd
+++ player/charMovement.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 400.0
+const JUMP_VELOCITY = -800.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = 1500
 
 
 func _physics_process(delta):
@@ -26,3 +26,8 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body):
+	global_position.x = 0
+	global_position.y = 0
